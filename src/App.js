40dbbd899ec9e2ab0f213143fc9a06ee8cd64359
rawkintrevo <trevor.d.grant@gmail.com>
2024-04-30 10:24:43 -0500

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login/Login';
 import Sidebar from './components/Sidebar/Sidebar';
 import AccountPage from './components/AccountPage/AccountPage';
 import MessagesView from './components/MessagesView/MessagesView';
-import Header from './components/Header/Header';
+// import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -26,22 +27,49 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 function App() {
-  return (
-      <Router>
-        <div className="App">
-          <Header />
-          <div className="main-content">
-            <Sidebar />
-            <Routes>
-              <Route path="/" element={<Login />} />
-              <Route path="/account" element={<AccountPage />} />
-              <Route path="/chat/:chatId" element={<MessagesView />} />
-            </Routes>
-          </div>
-            <Footer />
-        </div>
-      </Router>
-  );
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        // Add Firebase auth listener here to set isLoggedIn
+        const auth = getAuth();
+        const unsubscribe = auth.onAuthStateChanged(user => {
+            setIsLoggedIn(!!user);
+            setUser(user);
+        });
+        return () => unsubscribe();
+    }, []);
+
+    return (
+        <Router>
+            <div className="App">
+                {/*<Header />*/}
+                {isLoggedIn ? (
+                    <div className="container-fluid">
+                        <div className="row">
+                            <div className="col-md-3">
+                                <Sidebar user={user} />
+                            </div>
+                            <div className="col-md-9">
+                                <Routes>
+                                    <Route path="/account" element={<AccountPage />} />
+                                    <Route path="/chat/:chatId" element={<MessagesView />} />
+                                    <Route path="/chat" element={<MessagesView />} />
+                                    <Route path="*" element={<Navigate to="/chat" />} />
+                                </Routes>
+                            </div>
+                        </div>
+                    </div>
+                ) : (
+                    <Routes>
+                        <Route path="/" element={<Login />} />
+                        <Route path="*" element={<Navigate to="/" />} />
+                    </Routes>
+                )}
+                <Footer />
+            </div>
+        </Router>
+    );
 }
 
 export default App;
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
-import { ListGroup } from 'react-bootstrap';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
+import { Nav, Dropdown } from 'react-bootstrap';
+import {getAuth, signOut} from "firebase/auth";
 
-function Sidebar() {
+
+function Sidebar({ user }) {
+
+    //eslint-disable-next-line
+    const [displayName, setDisplayName] = useState(user ? user.displayName : '');
     // This function is a stub for fetching historical chats
     const fetchChats = () => {
         return [
@@ -12,20 +17,43 @@ function Sidebar() {
         ];
     };
 
+    const handleSignOut = () => {
+        const auth = getAuth();
+        signOut(auth).then(() => {
+            console.log("User signed out successfully");
+            // Optionally, redirect the user to the login page or handle state updates
+        }).catch((error) => {
+            console.error("Sign out error", error);
+        });
+    };
     const chats = fetchChats();
 
     return (
-        <div className="sidebar">
-            <ListGroup>
-                <ListGroup.Item>
-                    <Link to="/account">Account Page</Link>
-                </ListGroup.Item>
+        <div className="d-flex flex-column flex-shrink-0 p-3 bg-light" style={{ width: "280px", height: "100vh" }}>
+            <Link to="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+                <span className="fs-4">Sidebar</span>
+            </Link>
+            <hr />
+            <Nav className="flex-column mb-auto">
                 {chats.map(chat => (
-                    <ListGroup.Item key={chat.id}>
-                        <Link to={`/chat/${chat.id}`}>{chat.name}</Link>
-                    </ListGroup.Item>
+                    <Nav.Link as={Link} to={`/chat/${chat.id}`} key={chat.id} className="text-dark">
+                        {chat.name}
+                    </Nav.Link>
                 ))}
-            </ListGroup>
+            </Nav>
+            <hr />
+            <Dropdown>
+                <Dropdown.Toggle as={Nav.Link} className="text-dark d-flex align-items-center" id="dropdown-account">
+                    <strong>{displayName}</strong>
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                    <Dropdown.Item as={Link} to="/account">API Keys</Dropdown.Item>
+                    <Dropdown.Item as={Link} to="/account">Create a bot</Dropdown.Item>
+                    <Dropdown.Item as={Link} to="/account">Profile</Dropdown.Item>
+                    <Dropdown.Divider />
+                    <Dropdown.Item as="button" onClick={handleSignOut}>Sign out</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
         </div>
     );
 }
