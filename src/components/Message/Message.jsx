3f4a7cd362d1f3017690ee768998fb64e1416587
rawkintrevo <trevor.d.grant@gmail.com>
2024-05-08 16:40:33 -0500

    [HOTFIX] Clean up margins

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -4,7 +4,13 @@ import { ListGroupItem } from 'react-bootstrap';
 
 function Message({ msg }) {
     return (
-        <ListGroupItem className="message-item" style={{ borderRadius: '15px', padding: '10px', backgroundColor: '#f0f0f0', margin: '5px', maxWidth: '75%', alignSelf: msg.sender === 'You' ? 'flex-end' : 'flex-start' }}>
+        <ListGroupItem className="message-item"
+                       style={{ borderRadius: '15px',
+                           padding: '10px',
+                           backgroundColor: '#f0f0f0',
+                           margin: '5px',
+                           maxWidth: '95%',
+                           alignSelf: msg.sender === 'You' ? 'flex-end' : 'flex-start' }}>
             {msg.error ? (
                 <>
                     <strong>ERROR</strong>: {msg.error}<br />
