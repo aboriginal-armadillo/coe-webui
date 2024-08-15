969de96e4c86b73614e43295ed6486d630d4515e
rawkintrevo <trevor.d.grant@gmail.com>
2024-08-15 13:58:02 -0500

    refactored workflowsview

---
+++ src/components/WorkflowsView/WorkflowControls.jsx
@@ -0,0 +1,16 @@
+// src/components/WorkflowsView/WorkflowControls.jsx
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+const WorkflowControls = ({ addNode, runWorkflow }) => {
+    return (
+        <div>
+            <Button className="me-2" onClick={() => addNode('User Input')}>Add User Input Node</Button>
+            <Button className="me-2" onClick={() => addNode('LLM Node')}>Add LLM Node</Button>
+            <Button className="me-2" disabled onClick={() => addNode('Tool')}>Add Tool Node</Button>
+            <Button className="me-2" onClick={runWorkflow}>Run</Button>
+        </div>
+    );
+};
+
+export default WorkflowControls;
