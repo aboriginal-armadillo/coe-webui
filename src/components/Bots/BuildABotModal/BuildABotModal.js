d9ab14d261ba729b7d48e38285df2d593a7989ee
Trevor Grant <trevor.d.grant@gmail.com>
2024-08-15 21:20:50 -0500

    add llm to node

--- src/components/Bots/BuildABotModal/BuildABotModal.js
+++ src/components/Bots/BuildABotModal/BuildABotModal.js
@@ -2,14 +2,20 @@ import React from 'react';
 import { Modal, Button } from 'react-bootstrap';
 import BuildABot from '../BuildABot/BuildABot';
 
-function BuildABotModal({ show, onHide, botData, user, isWorkflowBot = false, workflowId = null }) {
+function BuildABotModal({ show, onHide, botData, user,
+                            isWorkflowBot = false,
+                            workflowId = null,
+                        nodeId}) {
     return (
         <Modal show={show} onHide={onHide} size="lg">
             <Modal.Header closeButton>
                 <Modal.Title>{botData ? "Edit Bot" : "Create Bot"}</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <BuildABot user={user} botData={botData} isWorkflowBot={isWorkflowBot} workflowId={workflowId} />
+                <BuildABot user={user} botData={botData}
+                           isWorkflowBot={isWorkflowBot}
+                           workflowId={workflowId}
+                           nodeId={nodeId}/>
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="secondary" onClick={onHide}>
